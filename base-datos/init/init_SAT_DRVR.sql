7828c10387e00bfdaaeb8d84481fcffff93c61cc
Unknown <mcamara75@yahoo.es>
2017-10-29 01:35:17 -0300

    Se agrega script con la inserción en tabla SAT_DRVR Además de elimina sequence SAT_DRVR_ID, ya que los registros en dicha tabla, se deben crear en duro

---
+++ base-datos/init/init_SAT_DRVR.sql
@@ -0,0 +1,5 @@
+INSERT INTO SAT_DRVR(DRVR_ID, DRVR_NOMB, DRVR_CLASS, AUDI_CREA)
+    VALUES(1, 'ORACLE', 'oracle.jdbc.OracleDriver', systimestamp );
+    
+INSERT INTO SAT_DRVR(DRVR_ID, DRVR_NOMB, DRVR_CLASS, AUDI_CREA)
+    VALUES(2, 'SQLSERVER', 'com.microsoft.sqlserver.jdbc.SQLServerDriver', systimestamp );
