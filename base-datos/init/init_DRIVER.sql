4702ed9552cdad519df39290050e54d0f69ffba1
Unknown <mcamara75@yahoo.es>
2017-11-05 22:43:07 -0300

    Actualización scripts de inicialización

---
+++ base-datos/init/init_DRIVER.sql
@@ -0,0 +1,5 @@
+INSERT INTO DRIVER(NUM_DRIVER, NOM_DRIVER, CLASS_DRIVER)
+    VALUES(1, 'ORACLE', 'oracle.jdbc.OracleDriver' );
+    
+INSERT INTO DRIVER(NUM_DRIVER, NOM_DRIVER, CLASS_DRIVER)
+    VALUES(2, 'SQLSERVER', 'com.microsoft.sqlserver.jdbc.SQLServerDriver');
