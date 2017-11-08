fee88d0a8899e82084c6d269956dbf0c48883be6
mcamara75 <mcamara75@yahoo.es>
2017-11-07 21:21:31 -0300

    Se agrega Google Guava para manejar el Cache los token de sesión

--- satelite-ws/src/main/java/cl/majeanis/satelite/po/SesionPO.java
+++ satelite-ws/src/main/java/cl/majeanis/satelite/po/SesionPO.java
@@ -1,12 +1,14 @@
 package cl.majeanis.satelite.po;
 
 import java.time.LocalDateTime;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Repository;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 import cl.majeanis.satelite.to.modelo.SesionTO;
 import cl.majeanis.satelite.to.modelo.UsuarioTO;
@@ -17,36 +19,36 @@ public class SesionPO
 {
     private static final Logger logger = LogManager.getLogger(SesionPO.class);
 
-    private static final ConcurrentMap<String, SesionTO> cacheSesion = new ConcurrentHashMap<>();
-    
+    private static final Cache<String, SesionTO> cacheSesion = CacheBuilder.newBuilder().expireAfterWrite(24, TimeUnit.HOURS).build();
+
     public SesionTO crear(UsuarioTO usuario)
     {
         logger.debug("crear[INI] usuario={}", usuario);
-        
+
         String plainToken = usuario.getNombre() + ":" + usuario.getTipo().getCodigo();
-        logger.trace("crear: despues de crear el plainToken={}", plainToken );
+        logger.trace("crear: despues de crear el plainToken={}", plainToken);
 
         SesionTO sesion = new SesionTO();
-        sesion.setId( new Encrypted( plainToken ).text() );
+        sesion.setId(new Encrypted(plainToken).text());
         sesion.setUsuario(usuario);
         sesion.setFecha(LocalDateTime.now());
 
-        cacheSesion.putIfAbsent( sesion.getId(), sesion);
+        cacheSesion.put(sesion.getId(), sesion);
 
-        logger.debug("crear[FIN] sesion={}", sesion );
-        return sesion;        
+        logger.debug("crear[FIN] sesion={}", sesion);
+        return sesion;
     }
-    
+
     public SesionTO get(String id)
     {
-        logger.debug("get[INI] id={}", id );
-        
-        if( id == null )
+        logger.debug("get[INI] id={}", id);
+
+        if (id == null)
             return null;
 
-        SesionTO sesion = cacheSesion.get(id);
+        SesionTO sesion = cacheSesion.getIfPresent(id);
 
-        logger.debug("get[FIN] sesion={}",  sesion );
+        logger.debug("get[FIN] sesion={}", sesion);
         return sesion;
     }
 }
